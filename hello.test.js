d7286302fb165a3e3efa443855beb3e79a280f0a
xp1994723 <1335877763@qq.com>
2019-03-31 17:05:34 +0800

    Testing Travis CI

--- hello.test.js
+++ hello.test.js
@@ -4,7 +4,7 @@ var expect = require('chai').expect;
 var hello = require('./hello');
 
 describe('run hello.js', function() {
-  it('should return "Hello World"', function() {
-    expect(hello()).to.equal('Hello World');
+  it('should return "Hello node"', function() {
+    expect(hello()).to.equal('Hello node');
   });
 });
